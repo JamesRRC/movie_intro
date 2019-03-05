6682de8633474567853758ad1d8cfcdda8ce52b3
Shaedx <Jamesthewelder101@gmail.com>
2019-03-05 10:54:32 -0600

    search done

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,7 @@
 class SearchController < ApplicationController
   def results
     @query = params[:name]
-    @characters_found = Character.where('name LIKE ?', "%#{@query}%")
+    @characters = Character.where('name LIKE ?', "%#{@query}%")
+
   end
 end
