eff6f900a733723f2df17cd3a715bc9c3dff083a
Shaedx <Jamesthewelder101@gmail.com>
2019-03-05 11:01:05 -0600

    about page route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'search_results', to: 'search#results', as: 'search_results'
+  get 'about', to: 'about#about'
 
   root to: 'characters#index'
+
 end
